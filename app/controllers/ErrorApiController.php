bce727b7c1d32457f335ce6616c9161c5c43f5e4
Manuel Montoya <montoya.christensen@outlook.com>
2025-11-21 21:16:00 -0300

    Ultimando detalles: mejora en autenticacion y manejo de errores

--- app/controllers/ErrorApiController.php
+++ app/controllers/ErrorApiController.php
@@ -4,11 +4,13 @@ class ErrorApiController
     public function not_allowed($req, $res)// valida el verbo endpoint
     {
         // devuelve 405 si el method no es valido por ej si manda delete a /vendedores
-        return $res->json('Request Method not Allowed', 405);
+        return $res->json(['Error' => 'Request method not allowed for requested resource'], 405);
+
     }
 
     public function not_found($req, $res)
     {
-        $res->json('Route Not Found', 404);
+        return $res->json(['Error' => 'Route not found for requested resource'], 404);
+
     }
 }
--- app/middlewares/GuardApiMiddleware.php
+++ app/middlewares/GuardApiMiddleware.php
@@ -1,13 +1,37 @@
 <?php
 
-    class GuardApiMiddleware extends Middleware {
-        public function run($request, $response) {
-            if(!$request->user) {
+class GuardApiMiddleware extends Middleware
+{
+    public function run($request, $response)
+    {
+        // arreglo de rutas con acceso restringido
+        $protectedRoute = [
+            'POST' => ['ventas', 'vendedores'],
+            'PUT' => ['ventas/:id', 'vendedores/:id'],
+            'DELETE' => ['ventas/:id', 'vendedores/:id']
+        ];
+
+        $method = $_SERVER['REQUEST_METHOD'];
+        $resource = $_GET['resource'];
+
+        $flag = false;
+        if (isset($protectedRoute[$method]))
+            foreach ($protectedRoute[$method] as $pattern)
+                // busca en el arreglo 
+                if (str_starts_with($resource, explode('/:id', $pattern)[0]))
+                    $flag = true;
+
+        // si es de acceso restringido, valida credenciales de usuario
+        if ($flag):
+            if (!$request->user) {
                 header("WWW-Authenticate: Bearer realm='Access to the API'");
-                return $response->json("No autorizado", 401);
+                return $response->json(["Error" => "Authentication is required"], 401);
             }
-            else if(!in_array('administrador', $request->user->roles)) {
-                return $response->json("No tiene permisos suficientes", 403);
+
+            if (!in_array('administrador', $request->user->roles)) {
+                return $response->json(["Error" => "Request does not meet required permissions"], 403);
             }
-        }
+        endif;
     }
+}
+
